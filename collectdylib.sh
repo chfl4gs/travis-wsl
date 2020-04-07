9a9f06a84da44dd7ddde3130b4852d538d1e1178
chfl4gs <chbsd64@gmail.com>
2020-04-07 15:39:08 +0800

    macosx test

--- collectdylib.sh
+++ collectdylib.sh
@@ -1,1 +1,1 @@
-cd /usr/lib; find . -type f -name "*.dylib" -not iname '*/libobjc.A.dylib/*' -exec cp -v {} $TRAVIS_BUILD_DIR/qiling/examples/rootfs/x8664_macos/usr/lib/{} \;
+cd /usr/lib; find . -type f -name "*.dylib" -not -iname '*/libobjc.A.dylib/*' -exec cp -v {} $TRAVIS_BUILD_DIR/qiling/examples/rootfs/x8664_macos/usr/lib/{} \;
