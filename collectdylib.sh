babf6e55b72fcbd242724cd7e5d6437473d964ef
chfl4gs <chbsd64@gmail.com>
2020-04-07 16:43:49 +0800

    macosx test final

--- collectdylib.sh
+++ collectdylib.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find /usr/lib  -type f -name "*.dylib" -not -iname '*/libobjc.A.dylib/*' -exec cp -v {} qiling/examples/rootfs/x8664_macos/usr/lib/ \;
+find /usr/lib  -type f -name "*.dylib" ! -name "libobjc.A.dylib"  -exec cp -v {} qiling/examples/rootfs/x8664_macos/usr/lib/ \;
