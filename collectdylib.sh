df1c8468cb337e8aa314cd0c671daa21be398b56
chfl4gs <chbsd64@gmail.com>
2020-04-07 17:59:09 +0800

    macosx test final

--- collectdylib.sh
+++ collectdylib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /usr/lib -type f -name "*.dylib" -name "dyld" ! -name "libobjc.A.dylib" -exec install -D {} qiling/examples/rootfs/x8664_macos/usr/lib/{} \;
+find /usr/lib -type f -name "*.dylib" -name "dyld" -not -name "libobjc.A.dylib" -exec install -D {} qiling/examples/rootfs/x8664_macos/usr/lib/{} \;
