8cf757249c341e7f25994e5cadd05cd140a0857d
chfl4gs <chbsd64@gmail.com>
2020-04-07 17:46:04 +0800

    macosx test final

--- collectdylib.sh
+++ collectdylib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /usr/lib -type f -name "*.dylib" -name "dyld" ! -name "libobjc.A.dylib" -exec cp -v {} qiling/examples/rootfs/x8664_macos/usr/lib/ \;
+find /usr/lib -type f -name "*.dylib" -name "dyld" ! -name "libobjc.A.dylib" -exec cp -r -v {} 'qiling/examples/rootfs/x8664_macos/usr/lib/' \\;
